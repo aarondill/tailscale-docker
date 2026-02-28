9cda3c1961c8309fc7a66a7fde545cdaa22a965d
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-28 00:42:19 -0600

    fix: update generate-dependabot for when no dockerfiles

--- .github/generate-dependabot.sh
+++ .github/generate-dependabot.sh
@@ -1,51 +1,57 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Source: https://medium.com/@svenvanginkel/automating-dependabot-for-docker-compose-13acdff61133
-
-set -e
+set -euC
 
 tmpfile=$(mktemp)
 trap 'rm -f "$tmpfile"' EXIT
-# Header
-cat >"$tmpfile" <<'YAML'
+# File Header
+cat >|"$tmpfile" <<'YAML'
 version: 2
 updates:
-  - package-ecosystem: "docker-compose"
-    directories:
 YAML
 
 # Find and sort all docker-compose.yml directories
-find . -regex '.*/\(docker-\)?compose\(-[\w]+\)?\(?>\.[\w-]+\)?\.ya?ml' -print0 |
-  xargs -0 -n1 dirname |
-  sed 's|^\./||' |
-  sort |
-  while read -r dir; do
-    echo "      - \"/$dir\"" >>"$tmpfile"
-  done
-# Append the schedule block
-cat >>"$tmpfile" <<'YAML'
+readarray composes -t -d '' < <(find . -regex '.*/\(docker-\)?compose\(-[\w]+\)?\(?>\.[\w-]+\)?\.ya?ml' -print0)
+if [ "${#composes[@]}" -gt 0 ]; then
+  # Header
+  cat <<'YAML'
+  - package-ecosystem: "docker-compose"
+    directories:
+YAML
+  for file in "${composes[@]}"; do
+    d=$(dirname -- "$file")
+    printf '      - "%s"\n' "/${d#"./"}" # remove leading ./; replace with /
+  done | sort
+  # Append the schedule block
+  cat <<'YAML'
     schedule:
       interval: "daily"
 YAML
+fi >>"$tmpfile"
 
-# Header
-cat >>"$tmpfile" <<'YAML'
+# Find and sort all Dockerfile directories
+readarray dockerfiles -t -d '' < <(find . -iname 'Dockerfile' -print0)
+if [ "${#dockerfiles[@]}" -gt 0 ]; then
+  # Header
+  cat <<'YAML'
   - package-ecosystem: "docker"
     directories:
 YAML
-# Find and sort all Dockerfile directories
-find . -iname 'Dockerfile' -print0 |
-  xargs -0 -n1 dirname |
-  sed 's|^\./||' |
-  sort |
-  while read -r dir; do
-    echo "      - \"/$dir\"" >>"$tmpfile"
-  done
-
-# Append the schedule block
-cat >>"$tmpfile" <<'YAML'
+  for file in "${dockerfiles[@]}"; do
+    d=$(dirname -- "$file")
+    printf '      - "%s"\n' "/${d#"./"}" # remove leading ./; replace with /
+  done | sort
+  # Append the schedule block
+  cat <<'YAML'
     schedule:
       interval: "daily"
 YAML
+fi
+
+if [ "${#composes[@]}" -eq 0 ] && [ "${#dockerfiles[@]}" -eq 0 ]; then
+  echo "ℹ️ No docker-compose.yml or Dockerfile files found."
+  echo >|"$tmpfile"
+fi
 
 # Install if changed
 if ! [ -f .github/dependabot.yml ] || ! cmp -s "$tmpfile" .github/dependabot.yml; then
